9ab3f1238210e28c5fe3da8bc0b8331be5c53e16
dmitrybannikov <prestiz4321@gmail.com>
2023-08-10 09:27:00 -0700

    some loayote

--- lib/login/view/login_screen.dart
+++ lib/login/view/login_screen.dart
@@ -21,188 +21,188 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final size = MediaQuery.of(context).size;
+    // final isLarge = size.height > 640;
     return ScrollScaffold(
       body: Container(
-            width: double.maxFinite,
-            padding: EdgeInsets.only(
+        width: double.maxFinite,
+        padding: EdgeInsets.only(
           left: 20,
           top: 24,
           bottom: 24,
-              right: 20,
+          right: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Welcome Back",
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.left,
+              style: theme.textTheme.headlineLarge,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "Welcome Back",
+            Container(
+              width: 285,
+              margin: EdgeInsets.only(
+                left: 16,
+                top: 12,
+                right: 16,
+              ),
+              child: Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: CustomTextStyles.bodySmallBluegray700_1,
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 11,
+                  top: 62,
+                ),
+                child: Text(
+                  "Email",
                   overflow: TextOverflow.ellipsis,
                   textAlign: TextAlign.left,
-                  style: theme.textTheme.headlineLarge,
+                  style: theme.textTheme.labelLarge,
                 ),
-                Container(
-                  width: 285,
-                  margin: EdgeInsets.only(
-                    left: 16,
-                    top: 12,
-                    right: 16,
-                  ),
-                  child: Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
-                    style: CustomTextStyles.bodySmallBluegray700_1,
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      left: 11,
-                      top: 62,
-                    ),
-                    child: Text(
-                      "Email",
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.left,
-                      style: theme.textTheme.labelLarge,
-                    ),
-                  ),
-                ),
-                CustomTextFormField(
-                  controller: emailoneController,
-                  margin: EdgeInsets.only(
-                    left: 8,
-                    top: 10,
-                    right: 8,
-                  ),
-                  contentPadding: EdgeInsets.only(
-                    left: 16,
-                    top: 17,
-                    right: 16,
-                    bottom: 17,
-                  ),
-                  textStyle: theme.textTheme.bodySmall!,
-                  hintText: "Brandonelouis@gmail.com ",
-                  hintStyle: theme.textTheme.bodySmall!,
-                  textInputAction: TextInputAction.next,
-                  filled: true,
-                  fillColor: PrimaryColors.whiteA700,
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      left: 11,
-                      top: 14,
-                    ),
-                    child: Text(
-                      "Password",
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.left,
-                      style: CustomTextStyles.labelLargeOpenSans,
-                    ),
-                  ),
-                ),
-                CustomTextFormField(
-                  controller: groupnineController,
-                  margin: EdgeInsets.only(
-                    left: 8,
-                    top: 9,
-                    right: 8,
-                  ),
-                  suffix: Container(
-                    margin: EdgeInsets.only(
-                      left: 30,
-                      top: 13,
-                      right: 11,
-                      bottom: 13,
-                    ),
-                    child: CustomImageView(
-                      svgPath: ImageConstant.imgCheckmark,
-                    ),
-                  ),
-                  suffixConstraints: BoxConstraints(
-                    maxHeight: 50,
-                  ),
-                  obscureText: true,
-                  filled: true,
-                  fillColor: PrimaryColors.whiteA700,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    left: 11,
-                    top: 20,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CustomCheckboxButton(
-                        text: "Remember me",
-                        iconSize: 24,
-                        value: isCheckbox,
-                        padding: EdgeInsets.only(
-                          top: 3,
-                          bottom: 3,
-                        ),
-                        textStyle: CustomTextStyles.bodySmallBluegray300,
-                        onChange: (value) {
-                          isCheckbox = value;
-                        },
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: 5,
-                          bottom: 2,
-                        ),
-                        child: Text(
-                          "Forgot Password ?",
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.left,
-                          style: CustomTextStyles.bodySmallGray90001,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                LoginButton(
-                  text: "Login".toUpperCase(),
-                  onPressed: () {},
-                ),
-                LoginButton(
-                  loginButtonColors: LoginButtonColors.pink,
-                  text: "Sign in with Google".toUpperCase(),
-                  useGoogleIcon: true,
-                  onPressed: () {},
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 16,
-                    bottom: 5,
-                  ),
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "You don't have an account yet?  ",
-                          style: CustomTextStyles.bodySmallBluegray700,
-                        ),
-                        TextSpan(
-                          text: "Sign up",
-                          style:
-                              CustomTextStyles.bodySmallErrorContainer.copyWith(
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ],
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        
+            CustomTextFormField(
+              controller: emailoneController,
+              margin: EdgeInsets.only(
+                left: 8,
+                top: 10,
+                right: 8,
+              ),
+              contentPadding: EdgeInsets.only(
+                left: 16,
+                top: 17,
+                right: 16,
+                bottom: 17,
+              ),
+              textStyle: theme.textTheme.bodySmall!,
+              hintText: "Brandonelouis@gmail.com ",
+              hintStyle: theme.textTheme.bodySmall!,
+              textInputAction: TextInputAction.next,
+              filled: true,
+              fillColor: PrimaryColors.whiteA700,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 11,
+                  top: 14,
+                ),
+                child: Text(
+                  "Password",
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                  style: CustomTextStyles.labelLargeOpenSans,
+                ),
+              ),
+            ),
+            CustomTextFormField(
+              controller: groupnineController,
+              margin: EdgeInsets.only(
+                left: 8,
+                top: 9,
+                right: 8,
+              ),
+              suffix: Container(
+                margin: EdgeInsets.only(
+                  left: 30,
+                  top: 13,
+                  right: 11,
+                  bottom: 13,
+                ),
+                child: CustomImageView(
+                  svgPath: ImageConstant.imgCheckmark,
+                ),
+              ),
+              suffixConstraints: BoxConstraints(
+                maxHeight: 50,
+              ),
+              obscureText: true,
+              filled: true,
+              fillColor: PrimaryColors.whiteA700,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: 11,
+                top: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CustomCheckboxButton(
+                    text: "Remember me",
+                    iconSize: 24,
+                    value: isCheckbox,
+                    padding: EdgeInsets.only(
+                      top: 3,
+                      bottom: 3,
+                    ),
+                    textStyle: CustomTextStyles.bodySmallBluegray300,
+                    onChange: (value) {
+                      isCheckbox = value;
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 5,
+                      bottom: 2,
+                    ),
+                    child: Text(
+                      "Forgot Password ?",
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: CustomTextStyles.bodySmallGray90001,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            LoginButton(
+              text: "Login".toUpperCase(),
+              onPressed: () {},
+            ),
+            LoginButton(
+              loginButtonColors: LoginButtonColors.pink,
+              text: "Sign in with Google".toUpperCase(),
+              useGoogleIcon: true,
+              onPressed: () {},
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 16,
+                bottom: 5,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "You don't have an account yet?  ",
+                      style: CustomTextStyles.bodySmallBluegray700,
+                    ),
+                    TextSpan(
+                      text: "Sign up",
+                      style: CustomTextStyles.bodySmallErrorContainer.copyWith(
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/login/view/splash_screen.dart
+++ lib/login/view/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:jobfinder_figma_pro_v2/core/app_export.dart';
-import 'package:jobfinder_figma_pro_v2/widgets/custom_floating_button.dart';
 import 'package:jobfinder_ui/jobfinder_ui.dart';
 
 import '../../app/widgets/scroll_scaffold.dart';
@@ -14,75 +13,106 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScrollScaffold(
-      body: Container(
-        width: double.maxFinite,
-        padding: EdgeInsets.only(
-          left: 32,
-          right: 32,
-        ),
-        child: Column(
-          // mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                "Jobspot",
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.left,
-                style: theme.textTheme.titleMedium,
-              ),
-            ),
-            Center(
-              child: SizedBox(
-                height: 200,
-                child: SvgPicture.asset(
-                  ImageConstant.imgGroup75,
-                  fit: BoxFit.contain,
-                  // height: 300,
-                  // width: 310,
+    return SafeArea(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: SafeArea(
+              child: Scaffold(
+                backgroundColor: PrimaryColors.gray50,
+                body: Center(
+                  child: SingleChildScrollView(
+                    child: Container(
+                      width: double.maxFinite,
+                      padding: EdgeInsets.only(
+                        left: 32,
+                        top: 24,
+                        bottom: 24,
+                        right: 32,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 32),
+                          Center(
+                            child: SizedBox(
+                              height: 200,
+                              child: SvgPicture.asset(
+                                ImageConstant.imgGroup75,
+                                fit: BoxFit.contain,
+                                // height: 300,
+                                // width: 310,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 48),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "Find Your\n",
+                                  style: theme.textTheme.displayMedium,
+                                ),
+                                TextSpan(
+                                  text: "Dream Job Here!",
+                                  style: CustomTextStyles
+                                      .displayMediumOrangeA200
+                                      .copyWith(
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            "Explore all the most exciting job roles basedon your interest and study major.",
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.left,
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  shape: CircleBorder(),
+                  backgroundColor: PrimaryColors.indigo900,
+                  onPressed: () {},
+                  child: SizedBox.square(
+                    dimension: 60,
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: SvgPicture.asset(
+                        ImageConstant.imgArrowright,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
-            SizedBox(height: 78),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "Find Your\n",
-                    style: theme.textTheme.displayMedium,
+          ),
+          Positioned(
+            child: SafeArea(
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 32, right: 32),
+                  child: Text(
+                    "Jobspot",
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.left,
+                    style: theme.textTheme.titleMedium,
                   ),
-                  TextSpan(
-                    text: "Dream Job Here!",
-                    style: CustomTextStyles.displayMediumOrangeA200.copyWith(
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ],
+                ),
               ),
-              textAlign: TextAlign.left,
             ),
-            SizedBox(height: 15),
-            Text(
-              "Explore all the most exciting job roles basedon your interest and study major.",
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.left,
-              style: theme.textTheme.bodyMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: CustomFloatingButton(
-        height: 60,
-        width: 60,
-        backgroundColor: PrimaryColors.indigo900,
-        child: CustomImageView(
-          svgPath: ImageConstant.imgArrowright,
-          height: 30.0,
-          width: 30.0,
-        ),
+          ),
+        ],
       ),
     );
   }
